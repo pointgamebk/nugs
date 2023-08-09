940c625eede75800c5fdc044101ea2a885a2fa2f
Brian Hill <gamepointbk@gmail.com>
2023-08-09 17:11:06 -0400

    Next.config change
    ignoreBuildErrors = true

---
+++ .history/lib/models/community.model_20230809164924.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const communitySchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Nug",
+  },
+  nugs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Nug",
+    },
+  ],
+  members: [],
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default Community;
